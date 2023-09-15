6392f7417ee250ae0ca413a03c5eb7fa56b7d6b7
Pedro Lucas <pdrolucas.dev@gmail.com>
2023-09-14 23:11:34 -0300

    feat: create prompt select component

---
+++ web/src/components/prompt-select.tsx
@@ -0,0 +1,56 @@
+import { useEffect, useState } from 'react';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from './ui/select';
+import { api } from '@/lib/axios';
+
+interface Propmt {
+  id: string;
+  title: string;
+  template: string;
+}
+
+interface PromptSelectProps {
+  onPromptSelected: (template: string) => void;
+}
+
+export function PromptSelect(props: PromptSelectProps) {
+  const [prompts, setPrompts] = useState<Propmt[] | null>(null);
+
+  useEffect(() => {
+    api.get('/prompts').then((response) => {
+      setPrompts(response.data);
+    });
+  }, []);
+
+  function handlePromptSelected(promptId: string) {
+    const selectedPrompt = prompts?.find((prompt) => prompt.id === promptId);
+
+    if (!selectedPrompt) {
+      return;
+    }
+
+    props.onPromptSelected(selectedPrompt.template);
+  }
+
+  return (
+    <Select onValueChange={handlePromptSelected}>
+      <SelectTrigger>
+        <SelectValue placeholder="Selecione um prompt" />
+      </SelectTrigger>
+      <SelectContent>
+        {prompts?.map((prompt) => {
+          return (
+            <SelectItem value={prompt.id} key={prompt.id}>
+              {prompt.title}
+            </SelectItem>
+          );
+        })}
+      </SelectContent>
+    </Select>
+  );
+}
