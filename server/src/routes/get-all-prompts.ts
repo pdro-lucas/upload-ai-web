cc8f207a82e84743874b76d9c90d88b136349deb
Pedro Lucas <pdrolucas.dev@gmail.com>
2023-09-14 22:32:54 -0300

    refactor: remove params

--- server/src/routes/get-all-prompts.ts
+++ server/src/routes/get-all-prompts.ts
@@ -2,8 +2,9 @@ import { FastifyInstance } from 'fastify';
 import { prisma } from '../lib/prisma';
 
 export async function getAllPromptsRoute(app: FastifyInstance) {
-  app.get('/prompts', async (request, reply) => {
+  app.get('/prompts', async () => {
     const prompts = await prisma.prompt.findMany();
+
     return prompts;
   });
 }
